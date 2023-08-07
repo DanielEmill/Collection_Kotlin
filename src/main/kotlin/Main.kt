05058237d65e6f0bbecf5f3496900e0fa7b674f9
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-08-07 17:01:54 -0400

    Lista+Arreglo+lista mutable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val Arreglo1 = arrayOf<String>("Arreglo1", "Arreglo2", "Arreglo3", "Arreglo4")
+    val Lista1 = listOf("Lista1", "Lista2", "Lista3", "Lista4")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("Arreglo: ${Arreglo1[2]}")
+    println("Lista: ${Lista1[2]}")
+
+    println(Arreglo1.get(1))
+    println(Lista1.indexOf("Lista2"))
+
+    for (Arreglo1 in Lista1) {
+        println(Arreglo1)
+    }
+    // Tambien podemos añadir elemento o remover con:
+    val ListaMutable = mutableListOf("1", "2", "3", "4", "5", "6", "7", "8")
+
+    ListaMutable.add("Prueba1")
+    ListaMutable.remove("Prueba1")
+
 }
